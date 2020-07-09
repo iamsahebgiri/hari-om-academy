d00559ef3e943f46ebd1d2a1f5c9e5010be8613d
saheb Giri <sahebgiri578@gmail.com>
2020-07-09 19:19:34 +0530

    :snowflake: Fix: Added Pages and Routes

---
+++ pages/courses.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function courses() {
+  return (
+    <div>
+      Courses
+    </div>
+  )
+}
---
+++ pages/study.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function study() {
+  return (
+    <div>
+      Study
+    </div>
+  )
+}
