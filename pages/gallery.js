bcf1ea221b1b0be309d0951a626181c6cd479eef
saheb Giri <sahebgiri578@gmail.com>
2020-07-15 18:33:42 +0530

    :gear: Fixed: Lazy loading

--- pages/gallery.js
+++ pages/gallery.js
@@ -10,26 +10,26 @@ export default function gallery() {
       <Jumbotron title='Our Gallery' description='Capturing every happy moments.' />
       <div className='container'>
 
-        <img src='/gallery/1.jpg' alt='Hari om Academy' />
+        <img src='/gallery/1.jpg' loading='lazy' alt='Hari om Academy' />
 
-        <img src='/gallery/2.jpg' alt='Hari om Academy' />
+        <img src='/gallery/2.jpg' loading='lazy' alt='Hari om Academy' />
 
-        <img src='/gallery/3.jpg' alt='Hari om Academy' />
-        <img src='/gallery/4.jpg' alt='Hari om Academy' />
+        <img src='/gallery/3.jpg' loading='lazy' alt='Hari om Academy' />
+        <img src='/gallery/4.jpg' loading='lazy' alt='Hari om Academy' />
 
-        <img src='/gallery/5.jpg' alt='Hari om Academy' />
+        <img src='/gallery/5.jpg' loading='lazy' alt='Hari om Academy' />
 
-        <img src='/gallery/6.jpg' alt='Hari om Academy' />
+        <img src='/gallery/6.jpg' loading='lazy' alt='Hari om Academy' />
 
-        <img src='/gallery/7.jpg' alt='Hari om Academy' />
+        <img src='/gallery/7.jpg' loading='lazy' alt='Hari om Academy' />
 
-        <img src='/gallery/8.jpg' alt='Hari om Academy' />
+        <img src='/gallery/8.jpg' loading='lazy' alt='Hari om Academy' />
 
-        <img src='/gallery/9.jpg' alt='Hari om Academy' />
+        <img src='/gallery/9.jpg' loading='lazy' alt='Hari om Academy' />
 
-        <img src='/gallery/10.jpg' alt='Hari om Academy' />
+        <img src='/gallery/10.jpg' loading='lazy' alt='Hari om Academy' />
 
-        <img src='/gallery/11.jpg' alt='Hari om Academy' />
+        <img src='/gallery/11.jpg' loading='lazy' alt='Hari om Academy' />
       </div>
 
       <Footer />
