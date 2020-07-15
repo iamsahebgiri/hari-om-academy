f1e20cacf53a710eda2ba991c93d592945118080
saheb Giri <sahebgiri578@gmail.com>
2020-07-15 20:10:29 +0530

    :rocket: Added: Started working on About

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Header from '../components/Header'
+import Jumbotron from '../components/Jumbotron'
+import Footer from '../components/Footer'
 
 export default function about() {
   return (
     <div>
-      <h1>About</h1>
+      <Header />
+      <Jumbotron title="About us" description='Hari om academy is built with passion.' />
+      
+      <Footer />
     </div>
   )
 }
